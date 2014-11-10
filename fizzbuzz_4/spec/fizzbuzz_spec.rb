9058e281d0a839b993bcdcececc11baea82fd788
Shan Hasan <shan119@hotmail.co.uk>
2014-11-10 08:30:34 +0000

    Completed fizzbuzz kata

---
+++ fizzbuzz_4/spec/fizzbuzz_spec.rb
@@ -0,0 +1,50 @@
+require "fizzbuzz"
+
+describe "fizzbuzz" do
+  
+context "is divisible by" do
+
+  it "is divisible by three" do
+  expect(is_divisible_by_three?(3)).to be true
+  end
+
+  it "is divisible by five" do
+    expect(is_divisible_by_five?(5)).to be true
+  end
+
+  it "is divisible by fifteen" do
+    expect(is_divisible_by_fifteen?(15)).to be true
+  end
+end
+
+context "is NOT divisible by" do
+
+it "is divisible by three" do
+  expect(is_divisible_by_three?(1)).not_to be true
+  end
+
+  it "is divisible by five" do
+    expect(is_divisible_by_five?(1)).not_to be true
+  end
+
+  it "is divisible by fifteen" do
+    expect(is_divisible_by_fifteen?(1)).not_to be true
+  end
+end
+
+context "while playing the game" do
+
+  it "will return fizz when divisible by three" do
+    expect(fizzbuzz?(3)).to eq "Fizz"
+  end
+
+  it "will return fizz when divisible by three" do
+    expect(fizzbuzz?(5)).to eq "Buzz"
+  end
+
+  it "will return fizz when divisible by three" do
+    expect(fizzbuzz?(15)).to eq "Fizzbuzz"
+  end
+
+end
+end
